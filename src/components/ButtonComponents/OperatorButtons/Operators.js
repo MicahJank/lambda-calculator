7ad78db73f33f2d2722eb61a31f8d430aa615a74
MicahJank <MicahJank@users.noreply.github.com>
2019-07-26 14:12:11 -0500

    made it so the operator buttons show up for the calculator

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
+import OperatorButton from "./OperatorButton.js";
 
 //Import your array data to from the provided data file
+import data from "../../../data.js";
+
+const operatorsArray = data.operators;
 
 const Operators = () => {
   // STEP 2 - add the imported data to state
+  const [operatorState, setOperatorState] = useState(operatorsArray);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {operatorsArray.map(operatorObj => {
+         return <OperatorButton operator={operatorObj.char} />
+       })}
     </div>
   );
 };
+
+export default Operators;
