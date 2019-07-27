66c091ecc12626611ba0ae04ae45484dbc0e61cd
MicahJank <MicahJank@users.noreply.github.com>
2019-07-27 12:22:43 -0500

    finished making the display look right and got the color of the buttons right as well

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -3,8 +3,9 @@ import React from "react";
 const Display = props => {
   return (<div className="display">
   {/* Display any props data here */}
+  <h4 className="displayNumber">0</h4>
   {props.calculation}
-  0
+ 
   </div>)
 };
 
